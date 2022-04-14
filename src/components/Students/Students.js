3f4da8abf6451eb3e08ed668990b0af14e32bc85
Jeremy <jeremyeparker@yahoo.com>
2022-04-14 14:44:36 -0400

    commented out

--- src/components/Students/Students.js
+++ src/components/Students/Students.js
@@ -1,46 +1,46 @@
-import React, {useState, useEffect} from 'react';
-import Student from './Student';
-import axios from 'axios';
-import Spinner from '../common/Spinner';
+// import React, {useState, useEffect} from 'react';
+// import Student from './Student';
+// import axios from 'axios';
+// import Spinner from '../common/Spinner';
 
-const Students = (props) => {
-    const [students, setStudents] = useState([]);
-    const [loading, setLoading] = useState(true);
+// const Students = (props) => {
+//     const [students, setStudents] = useState([]);
+//     const [loading, setLoading] = useState(true);
 
-    useEffect(() =>{
-        const _getStudents = async () => {
-            setLoading(true);
-            const apiHost = process.env.REACT_APP_API_HOST || "http://localhost:8080"
-            try {
-              const res = await axios.get(`${apiHost}/api/students`) 
-              console.log(res.data);
-              setStudents(res.data);
-              setLoading(false);
-            } catch (err) {
-              console.log(err.message);
-              console.log(err.res)
-            }
-          }
-          _getStudents();
-    }, [setLoading,setStudents])
+//     useEffect(() =>{
+//         const _getStudents = async () => {
+//             setLoading(true);
+//             const apiHost = process.env.REACT_APP_API_HOST || "http://localhost:8080"
+//             try {
+//               const res = await axios.get(`${apiHost}/api/students`) 
+//               console.log(res.data);
+//               setStudents(res.data);
+//               setLoading(false);
+//             } catch (err) {
+//               console.log(err.message);
+//               console.log(err.res)
+//             }
+//           }
+//           _getStudents();
+//     }, [setLoading,setStudents])
 
-    const displayStudents = () =>{
-        return students.map(student => <Student student={student} key={student.id}/>)
-    }
+//     const displayStudents = () =>{
+//         return students.map(student => <Student student={student} key={student.id}/>)
+//     }
 
-    return loading ? (
-        <div style={{marginTop: '2em', justifyContent: 'center'}}>
-          <Spinner />
-        </div>
-      ) : (
-        <div style={{display: 'flex', flex: 1, flexDirection: 'column', width: '100%', justifyContent:'center', alignItems: 'center'}}>
-          <h2 style={{textAlign: 'center'}}>Students</h2>
-          <div style={{display: 'flex', flexDirection: "column", maxWidth: '720px', justifyContent: 'center'}}>
+//     return loading ? (
+//         <div style={{marginTop: '2em', justifyContent: 'center'}}>
+//           <Spinner />
+//         </div>
+//       ) : (
+//         <div style={{display: 'flex', flex: 1, flexDirection: 'column', width: '100%', justifyContent:'center', alignItems: 'center'}}>
+//           <h2 style={{textAlign: 'center'}}>Students</h2>
+//           <div style={{display: 'flex', flexDirection: "column", maxWidth: '720px', justifyContent: 'center'}}>
     
-          {displayStudents()}
-          </div>
-        </div>
-      )
-}
+//           {displayStudents()}
+//           </div>
+//         </div>
+//       )
+// }
 
-export default Students; 
+// export default Students; 
